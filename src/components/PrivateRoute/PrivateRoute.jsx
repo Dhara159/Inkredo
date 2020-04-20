9b909dbb322351a2a45b543725e45466f2708c49
Dhara Vihol <viholdhara@gmail.com>
2020-04-20 12:40:31 +0530

    Removed console.log

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -7,8 +7,6 @@ const PrivateRoute = ({ component: Component, currentUser, inverted, ...rest }) 
     // Show component page only if user is logges in
     // Show signin page if user is not logged in
     <Route {...rest} render={props => {
-      console.log("inverted", inverted);
-      console.log("currentuser", currentUser);
       return ((inverted && !currentUser) || (!inverted && currentUser) ?
         <Component {...props} />
         : <Redirect to="/" />)
